4fc8c5e6f2472ff217fb80dd2a2f52780f05fb6d
unknown <xuhang@cpu.edu.cn>
2019-07-05 13:10:06 +0800

    add aaa

--- network.py
+++ network.py
@@ -75,7 +75,7 @@ class GALPRUN():
 
         return model_list_f,model_list_c,bn,model_list_f_w,model_list_c_w
     def prunself(self):
-        mod=make_model(self,is_mask=True)
+        mod=self.make_model(self,is_mask=True)
         self.vggnet=mod
     def make_model(self,is_mask=True):
         model_list_f,model_list_c,bn,model_list_f_w,model_list_c_w=self.make_model_list()
@@ -146,9 +146,10 @@ class GALPRUN():
         print(nub_f_pruned/nub_f_all)
         print(nub_c_pruned/nub_c_all)
 
-    def train(self,epoch_time, lr=0.001,momentum=0.9, weight_decay=5e-4,distance_rate=0.1,train_add=False,distance_rate_add=0.01,distance_rate_mul=0.1,distance_rate_time=4,train_conv=True,train_linear=False):
+    def train(self,epoch_time, lr=0.001,momentum=0.9, weight_decay=5e-4,distance_rate=0.1,train_add=False,distance_rate_add=0.01,distance_rate_mul=0.1,distance_rate_time=4,train_conv=True,train_linear=False,log_path="train.log"):
         self.optimizer = optim.SGD(self.vggnet.parameters(), lr=lr,momentum=momentum, weight_decay=weight_decay)
         i_dis_t=0
+        f=open("data.txt","w")
         for epoch in range(epoch_time):
             st=time.time()
             i_dis_t+=1
@@ -170,7 +171,9 @@ class GALPRUN():
                 total += labels.size(0)
                 correct += predicted.eq(labels.data).cpu().sum()
             print('[epoch:%d] Loss: %.03f | Acc: %.3f%% '
-                            % (epoch + 1,sum_loss / (i + 1), 100. * correct / total))
+                            % (epoch + 1,sum_loss / (i + 1), 100.0 * correct / total))
+            f.write('%d,%.05f,%.5f%% '
+                            % (epoch + 1,sum_loss / (i + 1), 100.0 * correct / total))
             with torch.no_grad():
                 correct = 0
                 total = 0
@@ -181,10 +184,14 @@ class GALPRUN():
                     _, predicted = torch.max(outputs.data, 1)
                     total += labels.size(0)
                     correct += (predicted == labels).sum()
-                print('测试分类准确率为：%.3f%%' % (100 * correct / total))
+                print('测试分类准确率为：%.3f%%' % (100.0 * correct / total))
                 acc = 100. * correct / total
-                torch.save(self.vggnet.state_dict(), 'model/vggnet_%03d.pth' % (epoch + 1))
-                ed=time.time()
+            f.write('%.3f%%\r\n' % (100.0 * correct / total))
+
+
+            torch.save(self.vggnet.state_dict(), 'model/vggnet_%03d.pth' % (epoch + 1))
+            
+            ed=time.time()
             if train_conv:   
                 self.init_conv2d_distance_rate(distance_rate)
             if train_linear:   
@@ -204,3 +211,4 @@ class GALPRUN():
                     distance_rate=1.0-(1.0-distance_rate)*(1.0-distance_rate_mul)
                 i_dis_t=0
             print("Training Finished, TotalEPOCH=%d,Epochtime=%d" % (epoch,ed-st))
+        f.close()
